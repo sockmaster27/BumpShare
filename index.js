5e4895a37adcaa6aed95f5133d7cd9b622b6d40a
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2023-04-16 15:18:33 +0200

    Detect whether phone is facing up or down

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
+const enableButton = document.querySelector("button");
+enableButton.addEventListener("click", enableButton.remove);
+
+
+const upSound = new Audio("resources/up.mp3");
+const downSound = new Audio("resources/down.mp3");
+upSound.play();
+
+let preUp = false;
+let preDown = false;
+
 addEventListener("deviceorientation", (event) => {
     const { alpha, beta, gamma } = event;
 
-    const thresh = 1.3;
-    const flat = -thresh < beta && beta < thresh
+    const thresh = 5;
+    const facingUp = -thresh < beta && beta < thresh
+        && -thresh < gamma & gamma < thresh;
+    const facingDown = 180 - thresh < beta || beta < -180 + thresh
         && -thresh < gamma & gamma < thresh;
 
     const body = document.querySelector("body");
-    body.style.backgroundColor = flat ? "green" : "red";
+    body.style.backgroundColor = facingUp ? "green" : facingDown ? "blue" : "red";
+
+    if (facingUp && !preUp)
+        upSound.play();
+
+    if (facingDown && !preDown)
+        downSound.play();
+
+    preUp = facingUp;
+    preDown = facingDown;
+
 });

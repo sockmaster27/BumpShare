5aa1bed4c81f14c1f667f3e69a2d4bf97d4e54e3
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2023-04-16 20:05:03 +0200

    Detect table slamming

--- index.js
+++ index.js
@@ -4,30 +4,46 @@ enableButton.addEventListener("click", enableButton.remove);
 
 const upSound = new Audio("resources/up.mp3");
 const downSound = new Audio("resources/down.mp3");
-upSound.play();
+const bumpSound = new Audio("resources/bump.mp3");
 
-let preUp = false;
-let preDown = false;
+let facingUp = false;
+let facingDown = false;
 
 addEventListener("deviceorientation", (event) => {
     const { alpha, beta, gamma } = event;
 
     const thresh = 5;
-    const facingUp = -thresh < beta && beta < thresh
+    const facingUpNow = -thresh < beta && beta < thresh
         && -thresh < gamma & gamma < thresh;
-    const facingDown = 180 - thresh < beta || beta < -180 + thresh
+    const facingDownNow = (180 - thresh < beta || beta < -180 + thresh)
         && -thresh < gamma & gamma < thresh;
 
     const body = document.querySelector("body");
-    body.style.backgroundColor = facingUp ? "green" : facingDown ? "blue" : "red";
+    body.style.backgroundColor = facingUpNow ? "green" : facingDownNow ? "blue" : "red";
 
-    if (facingUp && !preUp)
+    if (facingUpNow && !facingUp)
         upSound.play();
 
-    if (facingDown && !preDown)
+    if (facingDownNow && !facingDown)
         downSound.play();
 
-    preUp = facingUp;
-    preDown = facingDown;
+    facingUp = facingUpNow;
+    facingDown = facingDownNow;
+});
 
+const bang = document.querySelector(".bang");
+addEventListener("devicemotion", (event) => {
+    const { x, y, z } = event.acceleration;
+    const amplitude = x * x + y * y + z * z;
+
+    if (facingUp && amplitude >= 5) {
+        bumpSound.play();
+        bang.classList.add("banged");
+
+        // If this is triggered multiple times in a row, the transitionend event acts weird.
+        // Only using setTimeout for timing the animation looks weird though, 
+        // so it's just fallback if the transitionend doesn't successfully remove the class.
+        bang.addEventListener("transitionend", () => bang.classList.remove("banged"), { once: true });
+        setTimeout(() => bang.classList.remove("banged"), 200);
+    }
 });

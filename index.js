6a79c3d1130d10fcbae6d5e6a4a660c49132a92d
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2023-04-17 21:18:04 +0200

    Introduce start button

--- index.js
+++ index.js
@@ -1,77 +1,87 @@
-const enableButton = document.querySelector("button");
-enableButton.addEventListener("click", enableButton.remove);
+const enableButton = document.querySelector(".start");
+enableButton.addEventListener("click", () => {
+    // Safari requires a user gesture to enable device orientation events.
+    DeviceOrientationEvent.requestPermission?.();
+    enableButton.remove();
+
+    const bang = document.querySelector(".bang");
+    bang.classList.remove("waiting");
+
+    init();
+});
 
 
 const upSound = new Audio("resources/up.mp3");
 const downSound = new Audio("resources/down.mp3");
 const bumpSound = new Audio("resources/bump.mp3");
 
+function init() {
+    let stabilityTimer;
+    let potentiallyStable = false;
+    let stable = false;
+    let facingUp = false;
 
-let stabilityTimer;
-let potentiallyStable = false;
-let stable = false;
-let facingUp = false;
+    addEventListener("deviceorientation", (event) => {
+        const { alpha, beta, gamma } = event;
+        const thresh = 5;
+        facingUp = -thresh < beta && beta < thresh
+            && -thresh < gamma & gamma < thresh;
+    });
 
-addEventListener("deviceorientation", (event) => {
-    const { alpha, beta, gamma } = event;
-    const thresh = 5;
-    facingUp = -thresh < beta && beta < thresh
-        && -thresh < gamma & gamma < thresh;
-});
+    addEventListener("devicemotion", (event) => {
+        const { x, y, z } = event.acceleration;
+        const amplitude = x * x + y * y + z * z;
 
-addEventListener("devicemotion", (event) => {
-    const { x, y, z } = event.acceleration;
-    const amplitude = x * x + y * y + z * z;
+        const thresh = 1;
+        const still = amplitude < thresh;
 
-    const thresh = 1;
-    const still = amplitude < thresh;
+        const stableNow = still && facingUp;
+        if (!stable && stableNow && !potentiallyStable)
+            stabilityTimer = setTimeout(becomeStable, 300);
+        else if (!stable && !stableNow && potentiallyStable)
+            clearTimeout(stabilityTimer);
 
-    const stableNow = still && facingUp;
-    if (!stable && stableNow && !potentiallyStable)
-        stabilityTimer = setTimeout(becomeStable, 300);
-    else if (!stable && !stableNow && potentiallyStable)
-        clearTimeout(stabilityTimer);
+        else if (stable && !stableNow && potentiallyStable)
+            stabilityTimer = setTimeout(becomeUnstable, 100);
+        else if (stable && stableNow && !potentiallyStable)
+            clearTimeout(stabilityTimer);
 
-    else if (stable && !stableNow && potentiallyStable)
-        stabilityTimer = setTimeout(becomeUnstable, 100);
-    else if (stable && stableNow && !potentiallyStable)
-        clearTimeout(stabilityTimer);
+        potentiallyStable = stableNow;
 
-    potentiallyStable = stableNow;
-
-    if (stable && amplitude >= 5)
-        bump();
-});
+        if (stable && amplitude >= 5)
+            bump();
+    });
 
 
-function becomeStable() {
-    stable = true;
-    const body = document.querySelector("body");
-    body.style.backgroundColor = "green";
-    upSound.play();
-}
+    function becomeStable() {
+        stable = true;
+        const body = document.querySelector("body");
+        body.style.backgroundColor = "green";
+        upSound.play();
+    }
 
-function bump() {
-    const bang = document.querySelector(".bang");
-    bumpSound.play();
-    bang.classList.add("banged");
+    function bump() {
+        const bang = document.querySelector(".bang");
+        bumpSound.play();
+        bang.classList.add("banged");
 
-    // If this is triggered multiple times in a row, the transitionend event acts weird.
-    // Only using setTimeout for timing the animation looks weird though, 
-    // so it's just fallback if the transitionend doesn't successfully remove the class.
-    bang.addEventListener("transitionend", () => bang.classList.remove("banged"), { once: true });
-    setTimeout(() => bang.classList.remove("banged"), 200);
-}
+        // If this is triggered multiple times in a row, the transitionend event acts weird.
+        // Only using setTimeout for timing the animation looks weird though, 
+        // so it's just fallback if the transitionend doesn't successfully remove the class.
+        bang.addEventListener("transitionend", () => bang.classList.remove("banged"), { once: true });
+        setTimeout(() => bang.classList.remove("banged"), 200);
+    }
 
-function becomeUnstable() {
-    stable = false;
-    const body = document.querySelector("body");
-    body.style.backgroundColor = "red";
-    downSound.play();
+    function becomeUnstable() {
+        stable = false;
+        const body = document.querySelector("body");
+        body.style.backgroundColor = "red";
+        downSound.play();
 
-    // Cancel last bump
-    bumpSound.pause();
-    bumpSound.currentTime = 0;
-    const bang = document.querySelector(".bang");
-    bang.classList.remove("banged");
+        // Cancel last bump
+        bumpSound.pause();
+        bumpSound.currentTime = 0;
+        const bang = document.querySelector(".bang");
+        bang.classList.remove("banged");
+    }
 }
